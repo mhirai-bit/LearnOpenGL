5fc3ed74213290b3a14254f064d1dc87e6fc85a0
Mikio <mkohri22@gmail.com>
2025-12-14 16:07:09 +0900

    add GLAD initialization check in main function

--- main.cpp
+++ main.cpp
@@ -17,5 +17,10 @@ int main() {
     }
     glfwMakeContextCurrent(window);
 
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
     return 0;
 }
