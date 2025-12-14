b2a0bf06e8076c9f76a231a93ff1142c4fb6a5a4
Mikio <mkohri22@gmail.com>
2025-12-14 17:41:51 +0900

    add vertex shader source and compilation logic

--- main.cpp
+++ main.cpp
@@ -2,6 +2,14 @@
 #include <glad/glad.h> 
 #include <GLFW/glfw3.h>
 
+const char *vertexShaderSource = 
+    "#version 330 core\n"
+    "layout(location = 0) in vec3 aPos;\n"
+    "void main()\n"
+    "{\n"
+    "   gl_Position = vec4(aPos, 1.0);\n"
+    "}\n";
+
 void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
     glViewport(0, 0, width, height);
 }
@@ -45,6 +53,18 @@ int main() {
     glGenBuffers(1, &VBO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    unsigned int vertexShader;
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+    int success; 
+    char infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
 
     while(!glfwWindowShouldClose(window)) {
         processInput(window);
