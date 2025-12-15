a6a724c0a4472ee823fa46ee9c45e0ed1738a679
Mikio <mkohri22@gmail.com>
2025-12-15 21:11:31 +0900

    update shaders to pass vertex color from vertex to fragment shader

--- main.cpp
+++ main.cpp
@@ -5,17 +5,20 @@
 const char *vertexShaderSource = 
     "#version 330 core\n"
     "layout(location = 0) in vec3 aPos;\n"
+    "out vec4 vertexColor;\n"
     "void main()\n"
     "{\n"
     "   gl_Position = vec4(aPos, 1.0);\n"
+    "   vertexColor = vec4(aPos * 0.5 + 0.5, 1.0);\n"
     "}\n";
 
 const char *fragmentShaderSource = 
     "#version 330 core\n"
     "out vec4 FragColor;\n"
+    "in vec4 vertexColor;\n"
     "void main()\n"
     "{\n"
-    "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+    "   FragColor = vertexColor;\n"
     "}\n";
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
